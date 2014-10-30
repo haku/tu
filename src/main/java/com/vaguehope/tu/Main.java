753234259913434a3e62d5bf571e8f2efaf2f305
haku <git@haku.me>
2014-10-30 17:38:44 +0000

    initial

---
+++ src/main/java/com/vaguehope/tu/Main.java
@@ -0,0 +1,14 @@
+package com.vaguehope.tu;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+	private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+
+	public static void main (final String[] args) {
+		LOG.info("tu desu~");
+	}
+
+}
